f57e23a3a166627691d138c8ecea2580a4593b93
WoodyYan <sbyan@CNsbyan.local>
2016-06-27 13:45:14 +0800

    添加了BoxContainer。

---
+++ src/main/java/com/thoughtworks/guessnumber/BoxContainer.java
@@ -0,0 +1,7 @@
+package com.thoughtworks.guessnumber;
+
+public class BoxContainer {
+    public String getResult(BoxContainer otherContainer) {
+        return null;
+    }
+}
--- src/test/java/com/thoughtworks/guessnumber/BoxContainerTest.java
+++ src/test/java/com/thoughtworks/guessnumber/BoxContainerTest.java
@@ -7,13 +7,15 @@ import static org.hamcrest.CoreMatchers.is;
 
 public class BoxContainerTest {
     @Test
-    public void shouldReturnTrueIfTwoContainerAreEqual() {
+    public void shouldReturn1A1BIfOneContainerIs1234OtherContainerIs1456() {
         //given
+        BoxContainer otherContainer = new BoxContainer();
 
         //when
-        boolean result = false;
+        BoxContainer boxContainer = new BoxContainer();
+        String result = boxContainer.getResult(otherContainer);
 
         //then
-        Assert.assertThat(result, is(true));
+        Assert.assertThat(result, is("1A1B"));
     }
 }
