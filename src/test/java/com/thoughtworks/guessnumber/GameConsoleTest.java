957ec9ebe6e001c755b3b61d6e4be03d047f34f0
WoodyYan <sbyan@CNsbyan.local>
2016-06-27 14:26:38 +0800

    添加了GameConsoleTest

--- src/test/java/com/thoughtworks/guessnumber/GameConsoleTest.java
+++ src/test/java/com/thoughtworks/guessnumber/GameConsoleTest.java
@@ -1,6 +1,9 @@
 package com.thoughtworks.guessnumber;
 
+import org.junit.Assert;
 import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
 
 public class GameConsoleTest {
     @Test
@@ -8,7 +11,9 @@ public class GameConsoleTest {
         //given
 
         //when
+        boolean result = false;
 
         //then
+        Assert.assertThat(result, is(true));
     }
 }
