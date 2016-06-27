28297cc33d1ec5e96bc3a105c9a8ae10cb706541
WoodyYan <sbyan@CNsbyan.local>
2016-06-27 14:30:47 +0800

    听见了GameConsole

---
+++ src/main/java/com/thoughtworks/guessnumber/GameConsole.java
@@ -0,0 +1,14 @@
+package com.thoughtworks.guessnumber;
+
+public class GameConsole {
+
+    private final BoxContainer boxContainer;
+
+    public GameConsole(BoxContainer boxContainer){
+        this.boxContainer = boxContainer;
+    }
+
+    public boolean isAnswer(BoxContainer boxContainer) {
+        return false;
+    }
+}
--- src/test/java/com/thoughtworks/guessnumber/GameConsoleTest.java
+++ src/test/java/com/thoughtworks/guessnumber/GameConsoleTest.java
@@ -3,15 +3,31 @@ package com.thoughtworks.guessnumber;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import static org.hamcrest.CoreMatchers.is;
 
 public class GameConsoleTest {
     @Test
     public void should() {
         //given
+        Set<Box> boxes = new HashSet<>();
+        boxes.add(new Box(1, Location.First));
+        boxes.add(new Box(2, Location.Second));
+        boxes.add(new Box(3, Location.Third));
+        boxes.add(new Box(4, Location.First));
+        BoxContainer boxContainer = new BoxContainer(boxes);
 
         //when
-        boolean result = false;
+        Set<Box> answerBoxes = new HashSet<>();
+        answerBoxes.add(new Box(1, Location.First));
+        answerBoxes.add(new Box(2, Location.Second));
+        answerBoxes.add(new Box(3, Location.Third));
+        answerBoxes.add(new Box(4, Location.First));
+        BoxContainer answer = new BoxContainer(answerBoxes);
+        GameConsole gameConsole = new GameConsole(answer);
+        boolean result = gameConsole.isAnswer(boxContainer);
 
         //then
         Assert.assertThat(result, is(true));
