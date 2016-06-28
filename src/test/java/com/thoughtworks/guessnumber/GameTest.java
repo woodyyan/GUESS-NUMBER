8e32e8dcd72f31506fec2bf6fa46a0275af8d7c6
WoodyYan <sbyan@CNsbyan.local>
2016-06-28 09:57:18 +0800

    修改了需求和任务列表。

---
+++ src/test/java/com/thoughtworks/guessnumber/GameTest.java
@@ -0,0 +1,14 @@
+package com.thoughtworks.guessnumber;
+
+import org.junit.Test;
+
+public class GameTest {
+    @Test
+    public void should_return_1A0B_when_input_number_is_1567() {
+        //given
+
+        //when
+
+        //then
+    }
+}
