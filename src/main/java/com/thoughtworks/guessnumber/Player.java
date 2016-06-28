16f38dc009bc127547f33e1969481802e95c9078
easystudio <easystudio@outlook.com>
2016-06-28 15:17:33 +0800

    完成了玩家回答一个答案。

---
+++ src/main/java/com/thoughtworks/guessnumber/Player.java
@@ -0,0 +1,8 @@
+package com.thoughtworks.guessnumber;
+
+class Player {
+    Integer[] answer() {
+
+        return new Integer[]{1, 2, 3, 4};
+    }
+}
--- src/test/java/com/thoughtworks/guessnumber/PlayerTest.java
+++ src/test/java/com/thoughtworks/guessnumber/PlayerTest.java
@@ -2,6 +2,19 @@ package com.thoughtworks.guessnumber;
 
 import org.junit.Test;
 
-public class PlayerTest {
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.CoreMatchers.is;
 
+public class PlayerTest {
+    @Test
+    public void should_return_a_valid_result_when_answer() {
+        //given
+        Player player = new Player();
+
+        //when
+        Integer[] result = player.answer();
+
+        //then
+        assertThat(result.length, is(4));
+    }
 }
